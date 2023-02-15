9e1a09093caece75ab400aea7d27d4f9c4fb8a0c
Jean Michel Kremer Oviedo <jeankremer20@gmail.com>
2023-02-15 19:36:06 -0300

    Nova alteração no signUpSchema

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,7 +17,7 @@ type FormDataProps = {
 
 const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome!'),
-  email: yup.string().required('Informe o e-mail').email('E-mail Inválido'),
+  email: yup.string().required('Informe o e-mail!').email('E-mail Inválido!'),
   password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos'),
   password_confirm: yup
     .string()
